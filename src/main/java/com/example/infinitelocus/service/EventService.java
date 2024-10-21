19b1b96b63acb7c143ca738811d8a50d64c9e763
pr19gya <pragya.shri2001@gmail.com>
2024-10-21 13:34:29 +0530

    .

--- src/main/java/com/example/infinitelocus/service/EventService.java
+++ src/main/java/com/example/infinitelocus/service/EventService.java
@@ -3,17 +3,21 @@ package com.example.infinitelocus.service;
 import com.example.infinitelocus.model.Event;
 import com.example.infinitelocus.repository.EventRepository;
 import com.example.infinitelocus.repository.UserRepository;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController
+@Service
 public class EventService {
     public EventRepository eventRepository;
     public UserRepository userRepository;
     public EventService(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
+    }
+    public UserRepository userRepository() {
+
     }
     public Event createEvent(Event event){
         return eventRepository.save(event);
@@ -24,16 +28,14 @@ public class EventService {
     public List<Event> getUpcomingEvents(LocalDateTime date){
         return eventRepository.findByDate(date);
     }
-    public Event saveEvent(String title,int id
+    public Event saveEvent(String title
             ,String username,String location,LocalDateTime date){
-        Event event= eventRepository.findEventById(id);
-        if(event) {
+        List<Event> event= eventRepository.findEventsByTitle(title);
+        if(event.size()==0) {
             System.out.print("No events found");
         }
         else{
-
-            event.add(new Event(title,username,));
-            userRepository.registerUser(username,title);
+            eventRepository.registerForEvent(title,username,location,date);
 
         }
 
--- src/main/java/com/example/infinitelocus/service/UserService.java
+++ src/main/java/com/example/infinitelocus/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.infinitelocus.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.infinitelocus.repository.EventRepository;
+import com.example.infinitelocus.repository.UserRepository;
+
+
+@Service
 public class UserService {
+    public EventRepository eventRepository;
+    public UserRepository userRepository;
+
+   
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
 }
